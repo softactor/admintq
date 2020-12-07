d39c7fab404414f202dbc71301ee42da6adfd9a3
softactor <tanveerqureshee1@gmail.com>
2020-12-07 18:33:28 +0600

    added top session name

--- top_sidebar.php
+++ top_sidebar.php
@@ -26,7 +26,7 @@
                 <li class="dropdown user user-menu">
                     <a href="#" class="dropdown-toggle" data-toggle="dropdown">
                         <img src="vendor/dist/img/user2-160x160.jpg" class="user-image" alt="User Image">
-                        <span class="hidden-xs">Alexander Pierce</span>
+                        <span class="hidden-xs"><?php echo $_SESSION['logged']['user_name']; ?></span>
                     </a>
                     <ul class="dropdown-menu">
                         <!-- Menu Footer-->
